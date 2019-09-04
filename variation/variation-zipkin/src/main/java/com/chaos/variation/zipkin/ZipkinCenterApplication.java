ba025d28f6eacb8daa61e328d2e10870319553c9
wangchangsheng005 <1577873508@qq.com>
2019-09-04 19:39:04 +0800

    增加 zipkin 链路跟踪

---
+++ variation/variation-zipkin/src/main/java/com/chaos/variation/zipkin/ZipkinCenterApplication.java
@@ -0,0 +1,16 @@
+package com.chaos.variation.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinCenterApplication.class, args);
+    }
+}
